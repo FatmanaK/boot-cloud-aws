35726e70a18a6b65fbb5b764a11f8bd8b81b5397
M Konda <m.konda@outlook.com>
2021-02-26 23:40:12 +0000

    AWS Integration first cut

---
+++ aws/src/main/java/com/chocolateminds/aws/AwsApplication.java
@@ -0,0 +1,25 @@
+package com.chocolateminds.aws;
+
+import com.amazonaws.services.s3.AmazonS3;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = ContextInstanceDataAutoConfiguration.class)
+public class AwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner(AmazonS3 amazonS3){
+        return args -> {
+            amazonS3.listBuckets().forEach( bucket -> {
+                System.out.println(bucket.getName());
+            });
+        };
+    }
+}
---
+++ aws/src/test/java/com/chocolateminds/aws/AwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chocolateminds.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
